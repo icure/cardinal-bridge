1005f4b162161c62d1742d56e37fe5e5deb88bbd
Luca Tremamunno <tremaluca96@gmail.com>
2026-04-10 14:09:19 +0200

    Add serializers module to allow handling tryAndRecover methods

--- server/src/commonMain/kotlin/com/icure/cardinal/bridge/config/SerializationConfig.kt
+++ server/src/commonMain/kotlin/com/icure/cardinal/bridge/config/SerializationConfig.kt
@@ -1,13 +1,20 @@
 package com.icure.cardinal.bridge.config
 
 import com.icure.cardinal.sdk.utils.Serialization
+import com.icure.utils.InternalIcureApi
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import kotlinx.serialization.json.Json
 
+@OptIn(InternalIcureApi::class)
 fun Application.configureSerialization() {
 	install(ContentNegotiation) {
-		json(Serialization.json)
+		json(Json {
+			ignoreUnknownKeys = false
+			serializersModule = Serialization.fullLanguageInteropJson.serializersModule
+			classDiscriminator = "kotlinType"
+		})
 	}
 }
